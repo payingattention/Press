48816887f96baac36b15c8b6c5794f071a501b7d
Jason Ellis <jason.ellis@yahoo.com>
2012-03-17 14:18:58 -0500

    Create new post form

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -1,4 +1,19 @@
 class EditorController < ApplicationController
   def index
+    # Show a list
+    # Click to edit, then edit one
+    # Click new to create, then create one
+    puts params
+    post = params :post
+
+    puts post
+  end
+
+  # Create new post method
+  def create
+    # Map all users out to a name, id pair for the select box
+    @all_users = User.all.map { |a| [a.display_name, a.id] }
+    # Instance our post object to set form defaults
+    @post = Post.new
   end
 end
