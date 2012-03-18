69b804cf41231dcc104df0c2d0fccb44e5ef6128
Jason Ellis <jason.ellis@yahoo.com>
2012-03-17 21:57:48 -0500

    Simple validation of required fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # To configure a different table name
   # set_table_name("some_other_table") OR change the class name and file name
 
+  validates_presence_of :email, :password, :salt
+
 end
