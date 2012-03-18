1eaa7eed27fe7d9565d2e20f69e76a52a5fc5996
Jason Ellis <jason.ellis@yahoo.com>
2012-03-17 22:17:34 -0500

    Beginnings of admin users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,26 @@ class Admin::UsersController < ApplicationController
 
   layout 'admin'
 
+  # LIST
   def index
+    @users = User.order('created_at ASC')
   end
+
+  # NEW
+  def new
+    @user = User.new
+  end
+
+  # CREATE
+  def create
+    @user = User.new params[:user]
+    if @user.save
+      flash[:success] = 'User Created.'
+      redirect_to :action => 'index'
+    else
+      render 'new'
+    end
+  end
+
+
 end
