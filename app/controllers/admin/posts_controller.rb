9dad1ff5e5b529360941e703754ef818394afb9e
Jason Ellis <jason.ellis@yahoo.com>
2012-03-18 16:51:42 -0500

    The beginnings of some home grown pagination that needs to be put into a helper

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -4,7 +4,19 @@ class Admin::PostsController < ApplicationController
 
   # LIST -- Shows a list of posts
   def index
+    limit = 10;
+    page = (params[:page].to_i - 1) || 1
+    @filter = params[:filter] || ''
+
     @posts = Post.order('go_live DESC')
+    @total_post_count = @posts.count
+    @posts = @posts.where(["title like ?", '%'+@filter+'%'] ) if @filter
+    @filtered_post_count = @posts.count
+    @posts = @posts.limit(limit)
+    @posts = @posts.offset(limit.to_i * page.to_i) if page > 0
+
+    @pagination_current_page = (page.to_i + 1) > 0 ? (page.to_i + 1) : 1
+    @pagination_number_of_pages = (@filtered_post_count / limit) +1
   end
 
   # NEW -- New post form method
