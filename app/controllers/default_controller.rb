9b2bb4df1b9e9a89d1175a7436cd024f460cd8d1
Jason Ellis <jason.ellis@yahoo.com>
2012-03-18 22:27:01 -0500

    Beginnings on the main page output.. It's pointing out a lot of issues with my importer..

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,13 +1,23 @@
 class DefaultController < ApplicationController
 
+  layout 'application'
+  #
   def index
-    #user = User.new
-    #user.email = 'palamedes@rocketmail.com'
-    #user.password = '12345'
-    #user.salt = 'qwerty'
-    #user.save
-    #
-    #user.delete
+    # Get the 5 latest
+    limit = 5;
+    page = (params[:page].to_i - 1) || 1
+    @filter = params[:filter] || ''
+
+    @posts = Post.order('go_live DESC')
+    @total_post_count = @posts.count
+    @posts = @posts.where(["title like ?", '%'+@filter+'%'] ) if @filter
+    @filtered_post_count = @posts.count
+    @posts = @posts.limit(limit)
+    @posts = @posts.offset(limit.to_i * page.to_i) if page > 0
+
+    @pagination_current_page = (page.to_i + 1) > 0 ? (page.to_i + 1) : 1
+    @pagination_number_of_pages = (@filtered_post_count / limit) +1
+
   end
 
 end
