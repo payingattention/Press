0b1244d46f6b26416de557b11b90db9f9b5cab9d
Jason Ellis <jason.ellis@yahoo.com>
2012-03-25 11:30:10 -0500

    Updating routes to allow for pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,17 @@ Press::Application.routes.draw do
     resources :users
     resources :import_wordpress
   end
-
+  # Admin route that gets us entry to the above namespace
   match 'admin/' => 'admin#index'
+  # Category route similar to wordpress
   match '/category/*category' => 'default#category'
+  # Tag route similar to wordpress
   match '/tag/*tag' => 'default#tag'
+  # Page route which gets us deeper into the main index page
+  match '/page/*page' => 'default#index'
+  # Default SEO URL (Permalink) route for showing pages and posts
   match '*seo_url' => 'default#show'
-
+  # Default
   root :to => 'default#index'
 
 end
