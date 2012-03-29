3a7703db9543e02f52a2b91105939dc0b31b7dfe
Jason Ellis <jason.ellis@yahoo.com>
2012-03-29 08:33:18 -0500

    Making posts controller use scaffolding like respond_to to handle json

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -19,6 +19,11 @@ class Admin::PostsController < ApplicationController
 
     @pagination_current_page = (page.to_i + 1) > 0 ? (page.to_i + 1) : 1
     @pagination_number_of_pages = (@filtered_post_count / limit) +1
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @posts }
+    end
   end
 
   # NEW -- New post form method
@@ -27,6 +32,11 @@ class Admin::PostsController < ApplicationController
     @all_users = User.all.map { |a| [a.display_name, a.id] }
     # Instance our post object to set form defaults
     @post = Post.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @post }
+    end
   end
 
   # CREATE -- Does the work of saving new post or back to new
@@ -34,11 +44,15 @@ class Admin::PostsController < ApplicationController
     # Instance new object
     @post = Post.new params[:post]
     # Save object
-    if @post.save
-      flash[:success] = 'Post Created.'
-      redirect_to :action => 'index'
-    else
-      render 'new'
+    respond_to do |format|
+      if @post.save
+        flash[:success] = 'Post Created.'
+        format.html { redirect_to [:admin, @post], notice: 'Post was successfully created.' }
+        format.json { render json: @post, status: :created, location: @post }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -60,12 +74,15 @@ class Admin::PostsController < ApplicationController
       redirect_to :action => 'index'
     end
 
-    # Update object
-    if @post.update_attributes params[:post]
-      flash[:success] = 'Post Edited.'
-      redirect_to :action => 'index'
-    else
-      render 'edit'
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        flash[:success] = 'Post Edited.'
+        format.html { redirect_to admin_posts_url, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -76,12 +93,10 @@ class Admin::PostsController < ApplicationController
       redirect_to :action => 'index'
     end
 
-    if @post.destroy
-      flash[:success] = 'Post Deleted.'
-      redirect_to :action => 'index'
-    else
-      flash[:error] = 'Post failed to delete.'
-      redirect_to :action => 'index'
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_posts_url }
+      format.json { head :no_content }
     end
   end
 
