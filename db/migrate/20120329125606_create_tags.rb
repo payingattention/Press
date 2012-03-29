9aae3608f22b1a85a0e881f7e7017500c4e86c90
Jason Ellis <jason.ellis@yahoo.com>
2012-03-29 08:13:22 -0500

    Adding tags (which are also categories)

---
+++ db/migrate/20120329125606_create_tags.rb
@@ -0,0 +1,12 @@
+class CreateAdminTags < ActiveRecord::Migration
+  def change
+    create_table :admin_tags do |t|
+      t.string    :name,                                                          :null => false
+      t.string    :seo_url,                                                       :null => false
+      t.text      :description
+      t.enum      :classification, :limit => [:tag, :category], :default => :tag, :null => false
+
+      t.timestamps
+    end
+  end
+end
