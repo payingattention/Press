f27b646b9f18a0b47289b8bb0f47d5df898bcc58
Jason Ellis <jason.ellis@yahoo.com>
2012-03-31 10:43:46 -0500

    More work to make the query string stick as you navigate around the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # breadcrumbs!
   def render_breadcrumbs options = { }
-    options[:home]            ||= { :name => 'Home', :link => { :action => 'index'} }
+    options[:filter]          ||= nil
+    options[:home]            ||= { :name => 'Home', :link => { :action => 'index', :query => options[:filter] } }
     options[:post]            ||= nil
     options[:category]        ||= options[:post].categories.first if options[:post].present?
     options[:tag]             ||= nil
-    options[:filter]          ||= nil
     options[:span_size]       ||= 'span9'
     options[:container]       ||= false
     options[:divider]         ||= '/'
@@ -14,9 +14,9 @@ module ApplicationHelper
 
     breadcrumbs = []
     breadcrumbs << link_to( options[:home][:name], options[:home][:link] )
-    breadcrumbs << link_to( options[:category].name.capitalize, '/category/' + options[:category].seo_url ) if options[:category].present?
-    breadcrumbs << link_to( options[:tag].name.capitalize, '/tag/' + options[:tag].seo_url ) if options[:tag].present?
-    breadcrumbs << link_to( options[:post].title, '/' + options[:post].seo_url ) if options[:post].present?
+    breadcrumbs << link_to( options[:category].name.capitalize, '/category/' + options[:category].seo_url + url_query ) if options[:category].present?
+    breadcrumbs << link_to( options[:tag].name.capitalize, '/tag/' + options[:tag].seo_url + url_query ) if options[:tag].present?
+    breadcrumbs << link_to( options[:post].title, '/' + options[:post].seo_url + url_query ) if options[:post].present?
 
     render :partial => 'shared/breadcrumbs',
        :locals => {
@@ -27,6 +27,11 @@ module ApplicationHelper
            :span_size         => options[:span_size],
            :render_container  => options[:container]
        }
+  end
+
+  # Append to the URL the query string if it exists
+  def url_query
+    (@query.present?)?'?query='+@query:''
   end
 
 end
