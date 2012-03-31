54bdd6dd0f4b1efbf6cf7e39e565168e182d26ea
Jason Ellis <jason.ellis@yahoo.com>
2012-03-31 11:50:58 -0500

    Adding documentation

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,5 +1,9 @@
 class Taxonomy < ActiveRecord::Base
 
+  # A Taxonomy is a keyword (letter, number, word, paragraph, icon..whatever) that is designed to provide some sort
+  # of identifier that itself has a classification in order to better organize things.
+  # Posts can have many different identifiers such as Tags, and Categories.  More over those taxonomies also reference
+  # back to the things they contain.
   has_and_belongs_to_many :posts
 
 end
