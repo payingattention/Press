ea5fe26cdcf15cff14476ae8e3cc19548b5bad3c
Jason Ellis <jason.ellis@yahoo.com>
2012-07-07 12:47:21 -0500

    Adding installing? method

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -6,4 +6,7 @@ class Setting < ActiveRecord::Base
   # Ultra simple "not null" validators
   # validates_presence_of :value
 
+  # Are we currently installing? (Meaning is it not installed yet)
+  scope :installing?, where(:key => :maintenance,:value => "-1")
+
 end
