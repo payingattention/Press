fea95d434a264a6f697862098b46dfe7df150ee8
Jason Ellis <jason.ellis@yahoo.com>
2012-07-29 12:43:35 -0500

    More unicorn tampering

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -8,9 +8,10 @@ APP_ROOT=/ruby/Press
 
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-# "bundle install --binstubs" version
-#CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+#CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+
+# "bundle install --binstubs" must be run for this to work
+CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 
 AS_USER=root
 
