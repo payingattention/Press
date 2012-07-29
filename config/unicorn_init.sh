af0498f2071855eddfe73705e2cbb4313ac4496f
Jason Ellis <jason.ellis@yahoo.com>
2012-07-29 12:35:47 -0500

    Updating unicorn init

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -3,10 +3,18 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/Press/current
+
+APP_ROOT=/ruby/Press
+
 PID=$APP_ROOT/tmp/pids/unicorn.pid
+
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+# "bundle install --binstubs" version
+#CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+
+#AS_USER=
+
+
 set -u
 
 OLD_PIN="$PID.oldbin"
