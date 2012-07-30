a4e987fa1632ec0f86afd346265abb022d24a6a5
Jason Ellis <jason.ellis@yahoo.com>
2012-07-30 18:54:57 -0500

    Using VERSION information instead of git trickery

--- app/controllers/admin/tools/software_update_controller.rb
+++ app/controllers/admin/tools/software_update_controller.rb
@@ -7,14 +7,8 @@ class Admin::Tools::SoftwareUpdateController < ApplicationController
 
   def index
 
-    @localVersionHash = %x{git rev-parse HEAD}.chomp
-    @remoteVersionHash = %x{git rev-parse origin/master}.chomp
     @localVersion = %x{cat VERSION}
-    @remoteVersion = "[Not sure the correct way to get this]"
-    @updateLog = %x{git log #{@localVersionHash}..HEAD --oneline}
-
-    @versionInformation = Curl::Easy.perform('https://raw.github.com/palamedes/Press/master/VERSION').body_str
-
+    @remoteVersion = Curl::Easy.perform('https://raw.github.com/palamedes/Press/master/VERSION').body_str
 
   end
 
