c62792c4e311e5fae3fd2dc19e1ca5faba8d011a
Jason Ellis <jason.ellis@yahoo.com>
2012-07-30 16:23:03 -0500

    Have our software know what version we are.. and what is out on github

--- app/controllers/admin/tools/software_update_controller.rb
+++ app/controllers/admin/tools/software_update_controller.rb
@@ -5,6 +5,10 @@ class Admin::Tools::SoftwareUpdateController < ApplicationController
   layout 'admin'
 
   def index
+    @localVersionHash = %x{git rev-parse HEAD}.chomp
+    @remoteVersionHash = %x{git rev-parse origin/master}.chomp
+    @localVersion = %x{cat VERSION}
+    @remoteVersion = %x{curl https://raw.github.com/palamedes/Press/master/VERSION}
   end
 
 end
