2542218a52a0abf66c99c7226b567e2f2ebaa01d
Jason Ellis <jason.ellis@yahoo.com>
2012-07-31 20:52:07 -0500

    Updated .gitignore and added updatelog to controller

--- app/controllers/admin/tools/software_update_controller.rb
+++ app/controllers/admin/tools/software_update_controller.rb
@@ -1,6 +1,8 @@
 class Admin::Tools::SoftwareUpdateController < AdminController
 
   def index
+    localVersionHash = %x{git rev-parse HEAD}.chomp
+    @updateLog = %x{git log #{localVersionHash}..HEAD --oneline}
 
     @localVersion = %x{cat VERSION}
     @remoteVersion = Curl::Easy.perform('https://raw.github.com/palamedes/Press/master/VERSION').body_str
