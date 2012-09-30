f7c010e4459a0b408104d969a364a1ca7073e832
Jason Ellis <jason.ellis@yahoo.com>
2012-09-30 12:09:52 -0500

    Adding more validators to posts based on type

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,14 +9,15 @@ class Post < ActiveRecord::Base
   # back to the things they contain.
   has_and_belongs_to_many :taxonomies
 
-  # A post may contain other posts as a "post" is just a container for some form of blob text with associated data.
-  # Posts can be "posts", "pages", "comments", "messages"..etc.
+  # A post may contain other posts, as a "post" is just a container for some form of blob text with associated data.
+  # Posts (content) can be "posts", "pages", "comments", "messages", "ads"..etc.
   has_many :posts
 
   # Add validation
   validates :go_live, :presence => true
-  validates :seo_url, :presence => { :message => "SEO Url can't be blank for Posts, if this isn't a post change it below" }, :if => :is_a_post?
-  validates :seo_url, :uniqueness => { :message => "It appears the SEO Url that you entered is already in use by another post" }, :if => :is_a_post?
+  validates :seo_url, :presence => { :message => "SEO Url can't be blank for Posts" }, :if => :is_a_post?
+  validates :seo_url, :presence => { :message => "SEO Url can't be blank for Pages" }, :if => :is_a_page?
+  validates :seo_url, :uniqueness => { :message => "It appears the SEO Url that you entered is already in use by another post or page" }, :if => :is_a_post? || :is_a_page?
 
   # Define some scoped helpers
   scope :ads, where(:object_type => :ad)
@@ -46,6 +47,10 @@ class Post < ActiveRecord::Base
   # Is this a post?
   def is_a_post?
     self.object_type == :post
+  end
+
+  def is_a_page?
+    self.object_type == :page
   end
 
 end
