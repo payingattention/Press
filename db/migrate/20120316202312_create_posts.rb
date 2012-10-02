9be44a333e9a2f6e80118affeff505724d33d968
Jason Ellis <jason.ellis@yahoo.com>
2012-10-02 14:12:28 -0500

    Adding some post types and a is_closable flag

--- db/migrate/20120316202312_create_posts.rb
+++ db/migrate/20120316202312_create_posts.rb
@@ -6,10 +6,11 @@ class CreatePosts < ActiveRecord::Migration
       t.text      "content"                   # Text blob
       t.string    "seo_url"                   # Perma link to this object
       t.string    "password", :limit => 40    # PW if set
-      t.enum      "object_type", :limit => [:post, :page, :comment, :message, :ad], :default => :post # Type of object, post, page, comment, message
+      t.enum      "object_type", :limit => [:post, :page, :comment, :message, :ad, :notice, :error, :success], :default => :post # Type of object
       t.enum      "state", :limit => [:draft, :published, :frozen], :default => :draft # published, draft, whatever
       t.boolean   "allow_comments", :default => true # are comments on this thing allowed?
       t.boolean   "is_sticky", :default => false # keep at top of matched listing?
+      t.boolean   "is_closable", :default => false # is this panel closable in a list?
       t.datetime  "go_live", :null => false   # Go live date
       t.datetime  "go_dead"                   # Go dead date
       t.timestamps
