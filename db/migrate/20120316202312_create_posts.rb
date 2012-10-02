b5d184b851ce3723769562b67149f9407bf83f75
Jason Ellis <jason.ellis@yahoo.com>
2012-10-02 16:27:23 -0500

    Adding a lot more post flags

--- db/migrate/20120316202312_create_posts.rb
+++ db/migrate/20120316202312_create_posts.rb
@@ -8,11 +8,14 @@ class CreatePosts < ActiveRecord::Migration
       t.string    "seo_url"                   # Perma link to this object
       t.string    "password", :limit => 40    # PW if set
       t.enum      "kind", :limit => [:post, :page, :comment, :message, :ad], :default => :post # Type of object
-      t.enum      "style", :limit => [:standard, :notice, :success, :error], :default => :standard
+      t.enum      "style", :limit => [:standard, :featured, :notice, :success, :error, :warning, :borderless], :default => :standard
       t.enum      "state", :limit => [:draft, :published, :frozen], :default => :draft # published, draft, whatever
       t.boolean   "allow_comments", :default => true # are comments on this thing allowed?
       t.boolean   "is_sticky", :default => false # keep at top of matched listing?
       t.boolean   "is_closable", :default => false # is this panel closable in a list?
+      t.boolean   "is_indexable", :default => true # is this content included in taxonomy index lists?
+      t.boolean   "is_searchable", :default => true # is this content included in site wide search?
+      t.boolean   "is_frontable", :default => true # does this content show up on the front page index list?
       t.datetime  "go_live", :null => false   # Go live date
       t.datetime  "go_dead"                   # Go dead date
       t.timestamps
@@ -20,5 +23,6 @@ class CreatePosts < ActiveRecord::Migration
     add_index("posts", "user_id")
     add_index("posts", "post_id")
     add_index("posts", "seo_url")
+    add_index("posts", "token")
   end
 end
