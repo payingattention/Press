e6dc316653f3fdf13ff4e4650d8c6b35c23414db
Jason Ellis <jason.ellis@yahoo.com>
2012-10-04 18:01:19 -0500

    Making user decorator do more work, and adding an admin link to the user panel

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -3,7 +3,24 @@ class UserDecorator < Draper::Base
 
   # Render the user panel
   def panel
-    h.render :partial => 'default/user_panel', :locals => { :user => model }
+    h.render :partial => 'default/user_panel', :locals => { :user => self }
+  end
+
+  # Logout Link
+  def logout_link
+    h.content_tag :li do
+      h.link_to h.destroy_user_session_path :method => :delete do
+        h.content_tag(:i, '', :class => 'icon-exclamation-sign') + " Logout"
+      end
+    end
+  end
+
+  def admin_link
+    h.content_tag :li do
+      h.link_to h.admin_index_path do
+        h.content_tag(:i, '', :class => 'icon-wrench') + " Site Administration"
+      end
+    end
   end
 
 end
