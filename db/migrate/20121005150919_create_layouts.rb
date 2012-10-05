cfb6b885d5be0b68ed45383735053e9611770fc0
Jason Ellis <jason.ellis@yahoo.com>
2012-10-05 14:59:47 -0500

    Updating layout and widget tables

--- db/migrate/20121005150919_create_layouts.rb
+++ db/migrate/20121005150919_create_layouts.rb
@@ -3,6 +3,7 @@ class CreateLayouts < ActiveRecord::Migration
     create_table :layouts do |t|
 
       t.string    "title",        :null => false                    # title of layout
+      t.text      "description",  :null => false
       t.boolean   "is_deletable", :null => false, :default => true  # is this one of the core layouts?
 
       t.timestamps
--- db/migrate/20121005151718_create_widgets.rb
+++ db/migrate/20121005151718_create_widgets.rb
@@ -2,11 +2,18 @@ class CreateWidgets < ActiveRecord::Migration
   def change
     create_table :widgets do |t|
 
-      t.string    "title",        :null => false                    # the pretty title of this widget
-      t.string    "token",        :null => false                    # how is this widget referenced
-      t.text      "description",  :null => false                    # whats this thing do?
-      t.text      "code",         :null => false                    # code that makes it happen
+      t.references :widget                          # Parent widget if this is a child
+      t.string    "title",        :null => false    # the pretty title of this widget
+      t.string    "token",        :null => false    # how is this widget referenced
+      t.text      "description",  :null => false    # whats this thing do?
+      t.text      "code",         :null => false    # code that makes it happen
+      t.boolean   "is_deletable", :null => false, :default => true  # is this one of the core widgets?
+
       t.timestamps
     end
+
+    add_index("widgets", "widget_id")
+    add_index("widgets", "token")
+
   end
 end
