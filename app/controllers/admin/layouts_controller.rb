325f70b61932807063af671d6289c6d9d444d155
Jason Ellis <jason.ellis@yahoo.com>
2012-10-05 15:39:45 -0500

    Adding edit action

--- app/controllers/admin/layouts_controller.rb
+++ app/controllers/admin/layouts_controller.rb
@@ -1,6 +1,6 @@
 class Admin::LayoutsController < AdminController
 
-  # LIST -- Shows a list of the content type [posts,pages,comments,messages,ads,etc...]
+  # LIST -- Shows a list of the layouts
   def index
 
     @layouts = LayoutDecorator.decorate(list_models Layout.order, [], 'created_at desc')
@@ -10,5 +10,16 @@ class Admin::LayoutsController < AdminController
       format.json { render json: @layouts }
     end
   end
+
+  # EDIT
+  def edit
+    # Instance the content
+    @layout = Layout.find_by_id params[:id]
+
+    unless @layout
+      redirect_to admin_layouts_path
+    end
+  end
+
 
 end
