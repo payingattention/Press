5bba48467c80e5caed740a7ae55c927832f852f3
Jason Ellis <jason.ellis@yahoo.com>
2012-10-05 15:36:23 -0500

    Adding layout<->widget association

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -1,5 +1,7 @@
 class Layout < ActiveRecord::Base
   attr_accessible :title
 
+  # A widget is an individual piece of a layout that gets down to the particulars of how to render something
+  has_and_belongs_to_many :widgets
 
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,3 +1,10 @@
 class Widget < ActiveRecord::Base
   attr_accessible :title, :token, :description, :code
+
+  # A layout is a wrapper object that describes how a page should be rendered.
+  has_and_belongs_to_many :layouts
+
+  # A widget may contain other widgets
+  has_many :widgets
+
 end
