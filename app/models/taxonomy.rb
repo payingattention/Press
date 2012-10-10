5b462113598d09a47ec93e5714803e3f619c6a03
Jason Ellis <jason.ellis@yahoo.com>
2012-10-10 12:50:41 -0500

    Adding mass assign and validation to taxonomy model

--- app/models/taxonomy.rb
+++ app/models/taxonomy.rb
@@ -1,5 +1,13 @@
 class Taxonomy < ActiveRecord::Base
 
+  # Mass assignable fields
+  attr_accessible :name, :seo_url, :description, :classification
+
+  # Add validation
+  validates :name,           :presence => true
+  validates :seo_url,        :presence => true
+  validates :classification, :presence => true
+
   # A Taxonomy is a keyword (letter, number, word, paragraph, icon..whatever) that is designed to provide some sort
   # of identifier that itself has a classification in order to better organize things.
   # Posts can have many different identifiers such as Tags, and Categories.  More over those taxonomies also reference
