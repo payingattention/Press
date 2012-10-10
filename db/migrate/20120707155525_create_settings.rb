9435a6ff25a48394de25481d9dc27749857859f1
Jason Ellis <jason.ellis@yahoo.com>
2012-10-09 19:52:00 -0500

    Bug

--- db/migrate/20120707155525_create_settings.rb
+++ db/migrate/20120707155525_create_settings.rb
@@ -24,8 +24,8 @@ class CreateSettings < ActiveRecord::Migration
         {'key' => 'url', 'value' => 'http://randomstringofwords.com', 'area' => :general, 'label' => 'Site Address (URL)', 'element' => :text, 'description' => '', 'values' => ''},
         {'key' => 'email', 'value' => '', 'area' => :general, 'label' => 'Admin Email Address', 'element' => :text, 'description' => 'This address is used as the from address for notification emails and general site contact.', 'values' => ''},
         {'key' => 'header_title', 'value' => '<span class="big">RandomStringOfWords</span><span class="micro">.com</span>', 'label' =>'Header Title HTML', 'area' => :general, 'label' => 'Header Title Output', 'element' => :text, 'description' => 'This is the top left header output', 'values' => ''},
-        {'key' => 'backup', 'value' => '1', 'label' => 'Local Backup', 'area' => :general, 'element' => :checkbox, 'description' => 'Create a local json backup of each item on save? (useful for making git backup)'}
-        {'key' => 'backup_location', 'value' => 'content', 'label' => 'Backup Location', 'area' => :general, 'element' => :text, 'description' => 'Local directory that holds json backup of all objects.'}
+        {'key' => 'backup', 'value' => '1', 'label' => 'Local Backup', 'area' => :general, 'element' => :checkbox, 'description' => 'Create a local json backup of each item on save? (useful for making git backup)'},
+        {'key' => 'backup_location', 'value' => 'content', 'label' => 'Backup Location', 'area' => :general, 'element' => :text, 'description' => 'Local directory that holds json backup of all objects.'},
 
     ]
 
