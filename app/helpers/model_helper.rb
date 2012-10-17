b0a04d946fb190977d2a7f136788ca35ae8722a5
Jason Ellis <jason.ellis@yahoo.com>
2012-10-17 16:43:07 -0500

    per google-fu should be hyphens, not underscores

--- app/helpers/model_helper.rb
+++ app/helpers/model_helper.rb
@@ -2,7 +2,7 @@ module ModelHelper
 
   # Wash all the crap out of the SEO url
   def self.strip_seo_url seo
-    seo.downcase.gsub(/\s/,'_').gsub(/[^0-9a-z\-\_]/,'')
+    seo.downcase.gsub(/\s/,'-').gsub(/[^0-9a-z\-\_]/,'')
   end
 
 end
