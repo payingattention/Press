c4a6d7561fb63e3d54dd175c14c23d73340bea35
Jason Ellis <jason.ellis@yahoo.com>
2012-10-18 10:11:09 -0500

    removing kind and token references

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,7 +3,7 @@ require 'model_helper'
 class Content < ActiveRecord::Base
 
   # Mass assignable fields
-  attr_accessible :layout_id, :text, :seo_url, :password, :kind, :format, :style, :state, :allow_comments, :is_sticky, :is_closable, :is_indexable, :is_searchable, :is_frontable, :go_live, :go_dead
+  attr_accessible :layout_id, :text, :seo_url, :password, :format, :style, :state, :allow_comments, :is_sticky, :is_closable, :is_indexable, :is_searchable, :is_frontable, :go_live, :go_dead
 
   # Contents ( posts, pages, comments, messages, ads etc.. ) must belong to a user
   belongs_to :user
@@ -23,41 +23,21 @@ class Content < ActiveRecord::Base
   # A piece of content belongs to a single layout that will be used to determine how to show that content.
   belongs_to :layout
 
-  # Before we create our content, generate a unique token id for it that can be used for reference if needed
-  before_validation :generate_token, :on => :create
   # Before we send to validation do any other preprocessing needed
   before_validation :preprocess
 
   # Add validation
-  validates :token,   :presence => true
   validates :go_live, :presence => true
-  validates :seo_url, :presence => { :message => "SEO Url can't be blank for Posts" }, :if => :is_a_post?
-  validates :seo_url, :presence => { :message => "SEO Url can't be blank for Pages" }, :if => :is_a_page?
-  validates :seo_url, :uniqueness => { :message => "It appears the SEO Url that you entered is already in use by another post or page" }, :if => :is_a_post? || :is_a_page?
+  validates :seo_url, :presence => { :message => "SEO Url can't be blank" }
+  validates :seo_url, :uniqueness => { :message => "It appears the SEO Url that you entered is already in use by another piece of content" }
 
   # After we have commit and saved, fire off any post processing commands
   after_commit :postprocess
-
-  # Define some scoped helpers
-  scope :ads, where(:kind => :ad)
-  scope :posts, where(:kind => :post)
-  scope :pages, where(:kind => :page)
-  scope :blocks, where(:kind => :block)
-  scope :comments, where(:kind => :comment)
-  scope :messages, where(:kind => :message)
 
   # Storage for the "parts" of a content
   @_header = nil
   @_body = nil
   @_tease = nil
-
-  # Generate a unique token and make sure its unique by testing for it
-  def generate_token
-    begin
-      token = SecureRandom.urlsafe_base64
-    end while Content.where(:token => token).exists?
-    self.token = token
-  end
 
   # Tags ( Taxonomies with a classification of tag )
   # content.tags returns all child taxonomies that are tags
@@ -71,25 +51,9 @@ class Content < ActiveRecord::Base
     taxonomies.all :conditions => { :classification => :category }
   end
 
-  # Comments ( Contents with an object kind of comment )
-  # content.comments returns all child contents that are comments
-  def comments
-    contents.all :conditions => { :kind => :comment }
-  end
-
   # Belongs to any taxonomy kind with id?
   def has_taxonomy? taxonomy_id
     taxonomies.all(:conditions => { :id => taxonomy_id }).present?
-  end
-
-  # Is this a post?
-  def is_a_post?
-    self.kind == :post
-  end
-
-  # is this a page?
-  def is_a_page?
-    self.kind == :page
   end
 
   # Return the body of the content
