44b0834473338cc712f10933b1b3a3f237bb429b
Jason Ellis <jason.ellis@yahoo.com>
2012-10-18 21:32:48 -0500

    Adding "system" to taxonomies

--- db/migrate/20120329125606_create_taxonomies.rb
+++ db/migrate/20120329125606_create_taxonomies.rb
@@ -1,10 +1,10 @@
 class CreateTaxonomies < ActiveRecord::Migration
   def change
     create_table :taxonomies do |t|
-      t.string    :name,                                                          :null => false
-      t.string    :seo_url,                                                       :null => false
+      t.string    :name,                                                                        :null => false
+      t.string    :seo_url,                                                                     :null => false
       t.text      :description
-      t.enum      :classification, :limit => [:tag, :category], :default => :tag, :null => false
+      t.enum      :classification,    :limit => [:tag, :category, :system], :default => :tag,   :null => false
 
       t.timestamps
     end
