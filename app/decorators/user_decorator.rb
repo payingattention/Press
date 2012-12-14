4defb96999347b08132f27ee15537f8117e52b84
Jason Ellis <jason.ellis@yahoo.com>
2012-12-14 11:12:40 -0600

    Updating logout link

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,12 +9,13 @@ class UserDecorator < Draper::Base
   # Logout Link
   def logout_link
     h.content_tag :li do
-      h.link_to h.destroy_user_session_path :method => :delete do
+      h.link_to h.destroy_user_session_path, :"data-method" => :delete do
         h.content_tag(:i, '', :class => 'icon-exclamation-sign') + " Logout"
       end
     end
   end
 
+  # Administration Link for admin only
   def admin_link
     if model.role == :admin || model.role == :owner
       h.content_tag :li do
